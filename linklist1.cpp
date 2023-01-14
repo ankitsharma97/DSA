75f4151d7e5ccfa11a4db9b0dac36bb1ce37ea84
ankit <ankit@kali>
2023-01-14 15:58:58 +0500

    add1

--- linklist1.cpp
+++ linklist1.cpp
@@ -1,8 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
+//expression of linklist
+struct node
+{
+    int a;
+    struct node *p;
+};
+
 int main()
 {
     int a,b;
     cin>>a>>b;
     cout<<a<< " "<<b;
-}g
+} 
